1920a019d9b9ee5b5444721c503d15b2b2b02acc
Katherine Blasman <b.katherine@hotmail.com>
2021-08-18 12:46:11 -0400

    Initial commit from gatsby: (https://github.com/juneate/s21-gatsby-blog.git)

---
+++ src/components/layout.js
@@ -0,0 +1,40 @@
+import * as React from "react"
+import { Link, useStaticQuery, graphql } from "gatsby"
+
+const Layout = ({ location, children }) => {
+  const rootPath = `${__PATH_PREFIX__}/`
+  const isRootPath = location.pathname === rootPath
+
+  const data = useStaticQuery(graphql`
+    query FooterQuery {
+      site {
+        siteMetadata {
+          social {
+            twitter
+          }
+        }
+      }
+    }
+  `)
+
+  const twitter = data.site.siteMetadata.social.twitter
+
+  return (
+    <div className="layout" data-is-root-path={isRootPath}>
+      <header className="site-header container">
+        <div className="logo"><Link to="/">Juneate</Link></div>
+      </header>
+      <main className="site-main container">{children}</main>
+      <footer className="site-footer">
+        <div className="container">
+          <p>Copyright © {new Date().getFullYear()}, Rocco Panacci.</p>
+          <ul>
+            <li><a href={`http://twitter.com/${twitter}`}>{`@${twitter}`}</a></li>
+          </ul>
+        </div>
+      </footer>
+    </div>
+  )
+}
+
+export default Layout
